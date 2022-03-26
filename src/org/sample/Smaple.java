7650f705919840b687c4bb2732019407ff2f941a
Ragulmojo <ragulm0612@gmail.com>
2022-03-26 22:04:40 +0530

    project sucess

---
+++ src/org/sample/Smaple.java
@@ -0,0 +1,29 @@
+package org.sample;
+
+public class Smaple {
+
+	public static void main(String[] args) {
+		String s = "Java applicationn";
+
+		//to find length
+		int a = s.length();
+		System.out.println(a);
+
+		//to check whether the string is empty or not
+
+		boolean e=	s.isEmpty();
+		System.out.println(e);
+
+		// to check particular character
+
+		char c =s.charAt(2);
+
+		System.out.println(c);
+// to find index positon
+	int i=	s.indexOf('c');
+	System.out.println(i);
+
+
+	}
+
+}
